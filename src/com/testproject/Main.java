6cbeadfaa48eb486bb89ace583aea9fa0810c8bd
Andras Torok <andras_torok@epam.com>
2019-08-13 12:11:22 +0200

    Initial commit.
    Also added the first method: sum().

---
+++ src/com/testproject/Main.java
@@ -0,0 +1,11 @@
+package com.testproject;
+
+import com.testproject.code.exercises.CodeExercise;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CodeExercise codeExercise = new CodeExercise();
+        System.out.println(codeExercise.sum(1, 2));
+    }
+}
---
+++ src/com/testproject/code/exercises/CodeExercise.java
@@ -0,0 +1,15 @@
+package com.testproject.code.exercises;
+
+public class CodeExercise {
+
+    /**
+     * Sum of Two Numbers
+     * Write a method that sums two numbers.
+     * @param i - first number
+     * @param j - second number
+     * @return - the sum of the given two numbers
+     */
+    public Integer sum(Integer i, Integer j) {
+        return i + j;
+    }
+}
