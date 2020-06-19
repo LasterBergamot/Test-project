d7342a51262e9d772284fcebcb61c8d6cd7569a0
Andras Torok <>
2020-06-19 09:08:58 +0200

    Added some stuff.

--- src/main/java/com/testproject/Main.java
+++ src/main/java/com/testproject/Main.java
@@ -1,15 +1,10 @@
 package com.testproject;
 
-import com.testproject.code.exercises.easy.EasyExercise;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new EasyExercise().getElement(List.of(3, 79), 2));
+
+        System.out.println("Heyy!");
+
     }
 }
--- src/main/java/com/testproject/code/wars/kyu/eight/KeepHydrated.java
+++
@@ -1,7 +0,0 @@
-package com.testproject.code.wars.kyu.eight;
-
-public class KeepHydrated {
-    public int Liters(double time)  {
-        return (int) Math.floor(time * 0.5d);
-    }
-}
