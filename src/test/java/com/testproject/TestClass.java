82d690f95aa1b7b5ec661912b93b48f09a2f296d
Andras Torok <>
2020-06-25 11:05:49 +0200

    Updated the Jenkinsfile with the test stage.
    Also, added dummy test.

---
+++ src/test/java/com/testproject/TestClass.java
@@ -0,0 +1,13 @@
+package com.testproject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestClass {
+
+    @Test
+    public void testMethod() {
+        assertEquals(4,2 + 2);
+    }
+}
